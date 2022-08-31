8fa979f4a5c71d67d16f700f7a56346acf51159b
srodriguez09 <110418699+srodriguez09@users.noreply.github.com>
2022-08-31 15:57:31 -0600

    asd1

--- WANovagro/WATickets/Controllers/BodegasController.cs
+++ WANovagro/WATickets/Controllers/BodegasController.cs
@@ -1,7 +1,11 @@
 ﻿using Newtonsoft.Json;
 using System;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.Entity;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -12,12 +16,115 @@ using System.Web.Http.Cors;
 using WATickets.Models;
 using WATickets.Models.Cliente;
 
-
 namespace WATickets.Controllers
+
 {   [Authorize]
     public class BodegasController : ApiController
     {
         ModelCliente db = new ModelCliente();
+        G G = new G();
+
+        [Route("api/Bodegas/InsertarSAP")]
+        public HttpResponseMessage GetExtraeDatos()
+        {
+            try
+            {
+                Parametros parametros = db.Parametros.FirstOrDefault(); //de aqui nos traemos los querys
+                var conexion = G.DevuelveCadena(db); //aqui extraemos la informacion de la tabla de sap para hacerle un query a sap
+
+                var SQL = parametros.SQLBodegas; //Preparo el query
+
+                SqlConnection Cn = new SqlConnection(conexion);
+                SqlCommand Cmd = new SqlCommand(SQL, Cn);
+                SqlDataAdapter Da = new SqlDataAdapter(Cmd);
+                DataSet Ds = new DataSet();
+                Cn.Open(); //se abre la conexion
+                Da.Fill(Ds, "Bodegas");
+
+                var Bodegas  = db.Bodegas.ToList();
+                foreach (DataRow item in Ds.Tables["ListaPrecios"].Rows)
+                {
+                    var cardCode = item["Codigo"].ToString();
+
+                    var Bodega = Bodegas.Where(a => a.CodSAP == cardCode).FirstOrDefault();
+
+                    if (Bodega == null) //Existe ?
+                    {
+                        try
+                        {
+                            Bodega = new Bodegas();
+                            Bodega.CodSAP = item["id"].ToString();
+                            Bodega.CodSuc = item["id"].ToString(); 
+                            Bodega.Nombre = item["Bodega"].ToString();
+                            
+                            db.Bodegas.Add(Bodega);
+                            db.SaveChanges();
+
+                        }
+                        catch (Exception ex1)
+                        {
+
+                            BitacoraErrores be = new BitacoraErrores();
+                            be.Descripcion = ex1.Message;
+                            be.StrackTrace = ex1.StackTrace;
+                            be.Fecha = DateTime.Now;
+                            be.JSON = JsonConvert.SerializeObject(ex1);
+                            db.BitacoraErrores.Add(be);
+                            db.SaveChanges();
+                        }
+                    }
+                    else
+                    {
+                        try
+                        {
+                            db.Entry(Bodegas).State = EntityState.Modified;
+
+                            Bodega.CodSuc = item["id"].ToString();
+                            Bodega.Nombre = item["Bodega"].ToString();
+
+
+                            db.SaveChanges();
+                        }
+                        catch (Exception ex1)
+                        {
+
+                            BitacoraErrores be = new BitacoraErrores();
+                            be.Descripcion = ex1.Message;
+                            be.StrackTrace = ex1.StackTrace;
+                            be.Fecha = DateTime.Now;
+                            be.JSON = JsonConvert.SerializeObject(ex1);
+                            db.BitacoraErrores.Add(be);
+                            db.SaveChanges();
+                        }
+
+                    }
+
+
+                }
+
+
+                Cn.Close(); //se cierra la conexion
+                Cn.Dispose();
+
+                return Request.CreateResponse(System.Net.HttpStatusCode.OK, "Procesado con exito");
+
+            }
+            catch (Exception ex)
+            {
+
+                BitacoraErrores be = new BitacoraErrores();
+                be.Descripcion = ex.Message;
+                be.StrackTrace = ex.StackTrace;
+                be.Fecha = DateTime.Now;
+                be.JSON = JsonConvert.SerializeObject(ex);
+                db.BitacoraErrores.Add(be);
+                db.SaveChanges();
+
+                return Request.CreateResponse(System.Net.HttpStatusCode.InternalServerError, ex);
+            }
+        }
+
+
         public HttpResponseMessage GetAll([FromUri] Filtros filtro)
         {
             try
