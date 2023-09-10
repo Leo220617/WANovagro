27bd55b821a27ba69f4244598b470129ec76ba74
srodriguez09 <srodriguez@dydconsultorescr.com>
2023-09-10 12:44:39 -0600

    ca

---
+++ WANovagro/WATickets/Controllers/ParametrosController.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WATickets.Models.Cliente;
+
+namespace WATickets.Controllers
+{
+    [Authorize]
+    public class ParametrosController : ApiController
+    {
+        ModelCliente db = new ModelCliente();
+        G G = new G();
+
+        public HttpResponseMessage GetAll()
+        {
+            try
+            {
+                var Parametros = db.Parametros.ToList();
+
+
+                return Request.CreateResponse(System.Net.HttpStatusCode.OK, Parametros);
+            }
+            catch (Exception ex)
+            {
+                BitacoraErrores be = new BitacoraErrores();
+                be.Descripcion = ex.Message;
+                be.StrackTrace = ex.StackTrace;
+                be.Fecha = DateTime.Now;
+                be.JSON = JsonConvert.SerializeObject(ex);
+                db.BitacoraErrores.Add(be);
+                db.SaveChanges();
+
+                return Request.CreateResponse(System.Net.HttpStatusCode.InternalServerError, ex);
+
+            }
+
+        }
+    }
+}
