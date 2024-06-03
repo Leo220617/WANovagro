0e85cfb6a90db155c25f8b9bb63dea9c42b18a1a
srodriguez09 <srodriguez@dydconsultorescr.com>
2024-06-03 13:38:27 -0600

    ca

---
+++ WANovagro/WATickets/Models/Cliente/BitacoraMargenes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WATickets.Models.Cliente
+{
+    public class BitacoraMargenes
+    {
+        public int id { get; set; }
+
+        public int idProducto { get; set; }
+
+        public decimal PrecioAnterior { get; set; }
+
+        public decimal PrecioNuevo { get; set; }
+
+        public DateTime Fecha { get; set; }
+    }
+}
