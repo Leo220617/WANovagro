2677314444a9ba0124cc0bc2dc81402c4b6aff19
srodriguez09 <110418699+srodriguez09@users.noreply.github.com>
2024-11-25 13:25:28 -0600

    ca

--- WANovagro/WATickets/Controllers/PromocionesController.cs
+++ WANovagro/WATickets/Controllers/PromocionesController.cs
@@ -203,7 +203,7 @@ namespace WATickets.Controllers
                     foreach (var item in promocion.Detalle)
                     {
                         var ClientePromo = db.ClientesPromociones.Where(a => a.idPromocion == Promo.id).FirstOrDefault();
-                        var PromoVieja = db.Promociones.Where(a => a.ItemCode == item.ItemCode && a.idListaPrecio == item.idListaPrecio && a.idCategoria == item.idCategoria && a.FechaVen > time).FirstOrDefault();
+                        var PromoVieja = db.Promociones.Where(a => a.ItemCode == item.ItemCode && a.idListaPrecio == item.idListaPrecio && a.idCategoria == item.idCategoria && a.FechaVen > time && a.Cliente == false).FirstOrDefault();
                         if (PromoVieja == null || promocion.Clientes.Count > 0)
                         {
                             Promociones det = new Promociones();
@@ -396,7 +396,7 @@ namespace WATickets.Controllers
                     foreach (var item in promocion.Detalle)
                     {
                         var ClientePromo = db.ClientesPromociones.Where(a => a.idPromocion == Promo.id).FirstOrDefault();
-                        var PromoVieja = db.Promociones.Where(a => a.ItemCode == item.ItemCode && a.idListaPrecio == item.idListaPrecio && a.idCategoria == item.idCategoria && a.FechaVen > time).FirstOrDefault();
+                        var PromoVieja = db.Promociones.Where(a => a.ItemCode == item.ItemCode && a.idListaPrecio == item.idListaPrecio && a.idCategoria == item.idCategoria && a.FechaVen > time && a.Cliente == false).FirstOrDefault();
                         if (PromoVieja == null)
                         {
                             Promociones det = new Promociones();
